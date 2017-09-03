ac1fd39d9a25f6850334d16e1e270366a6669d8d
Alexander Ivankov <AlexanderIvankoff@gmail.com>
2017-09-03 18:13:43 +0300

    add style for NotFoundPage

--- react-routing/src/components/NotFound.jsx
+++ react-routing/src/components/NotFound.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 
+import "./NotFound.less";
+
 export default class NotFound extends React.Component {
 	render() {
 		return(
 			<div className="NotFoundPage">
-				Page Not Found =(
+				- Page Not Found -
 			</div>
 		)
 	}
